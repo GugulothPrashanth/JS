6bcfeb5a14f81aa5b3f096be7a947646cf2eccef
GugulothPrashanth <142538046+GugulothPrashanth@users.noreply.github.com>
2024-07-31 02:32:07 +0000

    hoisting

---
+++ 01_basics/Hoisting.js
@@ -0,0 +1,16 @@
+
+
+getNumber();
+getNumber2();
+console.log(x);
+
+
+var x = 9;
+
+function getNumber () {
+    console.log("Hello Js");
+}
+
+var getNumber2 = () =>{
+    console.log("hello TS");
+}
